6de0be443ebce5794f102182373f4f2721e12c99
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-10 20:36:12 +0600

    5.1.ts

---
+++ Module-5/5.1.ts
@@ -0,0 +1,10 @@
+//  mongodd command line command
+/*  1.show dbs => to show data base
+2. to create new database => use databaseName
+3. to create collection => db.createCollection("collection Name")
+4. to add json data in a collection => db.getCollection("collection Name").insertOne({
+    json data
+})
+5. to see find the data => db.getCollection("collection name").find()
+
+  */
