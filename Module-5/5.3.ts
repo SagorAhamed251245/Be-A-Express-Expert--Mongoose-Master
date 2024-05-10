3a2a1e2755ce177ffeb46551cb0078caba7ee7af
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-05-10 21:39:58 +0600

    ssss

---
+++ Module-5/5.3.ts
@@ -0,0 +1,27 @@
+/* mongodb oparetor
+https://www.mongodb.com/docs/manual/reference/operator/query-comparison/
+
+Comparison Query Operators
+Comparison operators return data based on value comparisons.
+
+NOTE
+For details on a specific operator, including syntax and examples, click on the link to the operator's reference page.
+
+For comparison of different BSON type values, see the specified BSON comparison order.
+
+Name
+Description
+$eq
+Matches values that are equal to a specified value.
+$gt
+Matches values that are greater than a specified value.
+$gte
+Matches values that are greater than or equal to a specified value.
+$in
+Matches any of the values specified in an array.
+$lt
+Matches values that are less than a specified value.
+$lte = Matches values that are less than or equal to a specified value.
+$ne = Matches all values that are not equal to a specified value.
+$nin = Matches none of the values specified in an array.
+ */
---
+++ readme.md
@@ -0,0 +1,7 @@
+---
+
+/\* mongodb oparetor
+
+#### https://www.mongodb.com/docs/manual/reference/operator/query-comparison/
+
+rrr
